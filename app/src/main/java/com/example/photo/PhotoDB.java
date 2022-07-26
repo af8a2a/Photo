c1ff585ec637a55a94a3e582b017a0b7546cee9d
文韬 江 <1125209282@qq.com>
2022-07-26 23:55:47 +0800

    database

---
+++ app/src/main/java/com/example/photo/PhotoDB.java
@@ -0,0 +1,43 @@
+package com.example.photo;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class PhotoDB extends SQLiteOpenHelper {
+    public static final String CREATE_USER="create table USER("
+            +"id integer primary key autoincrement,"
+            +"username text,"
+            +"password text);";
+    private Context mContext;
+
+    public PhotoDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext=context;
+    }
+
+    public PhotoDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+        mContext=context;
+    }
+
+    public PhotoDB(@Nullable Context context, @Nullable String name, int version, @NonNull SQLiteDatabase.OpenParams openParams) {
+        super(context, name, version, openParams);
+        mContext=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_USER);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
