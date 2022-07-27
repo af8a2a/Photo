d9b9adc070cbc711918c04c9362d670a86983af1
文韬 江 <1125209282@qq.com>
2022-07-27 17:26:37 +0800

    login

--- app/src/main/java/com/example/photo/MainActivity.java
+++ app/src/main/java/com/example/photo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.photo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.InputType;
@@ -9,17 +13,45 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Boolean bPwdSwitch=false;
     private EditText etPwd;
+    private TextView signUp;
+    private EditText username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUp=findViewById(R.id.tv_sign_up);
         final ImageView ivPwdSwitch=findViewById(R.id.iv_pwd_switch);
         etPwd=findViewById(R.id.et_pwd);
+        username=findViewById(R.id.et_account);
+        //PhotoDB db=new PhotoDB(this,"USER.db",null,1);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PhotoDB db=new PhotoDB(MainActivity.this,"USER.db",null,1);
+                SQLiteDatabase userdb=db.getWritableDatabase();
+                String pwd=etPwd.getText().toString();
+                String user=username.getText().toString();
+                String[] targetCol={"username"};
+                Cursor c=userdb.query("USER",targetCol,user,null,null,null,null);
+                if(c==null){
+                    ContentValues val=new ContentValues();
+                    val.put("username",user);
+                    val.put("password",pwd);
+                    userdb.insert("USER",null,val);
+                }else{
+                    Toast.makeText(MainActivity.this,"已存在账号"+user,Toast.LENGTH_SHORT).show();
+                }
+                if(c!=null) c.close();
+                db.close();
+            }
+        });
         ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
