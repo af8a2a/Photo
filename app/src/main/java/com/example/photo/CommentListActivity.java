60fb0f5a1053bd84eac842e92421766a9cb49eda
文韬 江 <1125209282@qq.com>
2022-08-21 20:48:27 +0800

    comment

---
+++ app/src/main/java/com/example/photo/CommentListActivity.java
@@ -0,0 +1,14 @@
+package com.example.photo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_list);
+    }
+}
